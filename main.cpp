4d3f7ee653d370ce38cfdc9c92057da3520c274c
Niklas <niklas.bognar@gmail.com>
2024-04-02 15:12:15 +0100

    tested in main uses too much memory for laptop

--- main.cpp
+++ main.cpp
@@ -5,16 +5,11 @@
 
 int main() {
     auto start = std::chrono::high_resolution_clock::now();
-    auto data = readFile();
-    auto afterRead = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsedRead = afterRead - start;
-    calcAvr(data);
-    auto afterCalc = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> elapsedCalc = afterCalc - afterRead;
+    std::string input = "../data/measurements.txt";
+    evaluate(input);
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
-    std::cout << "Reading time: " << elapsedRead.count() << "s" << std::endl;
-    std::cout << "Calculation time: " << elapsedCalc.count() << "s" << std::endl;
+
     std::cout << "Execution time: " << elapsed.count() << "s" << std::endl;
 
     return 0;
