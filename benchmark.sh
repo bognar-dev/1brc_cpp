5d49d679283dec292836d5f2d860ccd02b75fdce
nikla <niklas.bognar@gmail.com>
2024-05-01 23:28:54 +0100

    loop-unrolling.c complete

--- benchmark.sh
+++ benchmark.sh
@@ -2,17 +2,13 @@
 export TIMEFORMAT='%2R'
 
 
-
-BRANCHNAMES=(
-    ""
-    "c-conversion"
-    "linear-search"
+PROGRAMS=(
     "hashmap"
-    "custom-parser"
+    "parse-double"
     "fread-chunks"
-    "unroll-parsing"
-    "parallelize"
-    "mmap"
+    "loop-unrolling"
+    #"parallelize"
+    #"mmap"
 )
 
 DESCRIPTIONS=(
@@ -32,32 +28,31 @@ line_counts=(1000 10000 100000 1000000 10000000 100000000 1000000000)
 # Create an array of filenames based on the line counts
 FILES=()
 for lines in "${line_counts[@]}"; do
-    FILES+=("file_${lines}_lines.txt")
+    FILES+=("../data/measurement_${lines}_lines.txt")
 done
 
 # Create a CSV file to store the runtimes
 echo "File,Program,Description,Runtime1,Runtime2,Runtime3" > runtimes.csv
-
+cd cmake-build-debug || exit
+make hashmap parse-double fread-chunks loop-unrolling
 # Loop over all files in the FILES array
 for FILE in "${FILES[@]}"; do
     if [ -f "$FILE" ]; then
-        for BRANCH in ${BRANCHNAMES}; do
-            git checkout "$BRANCH"
-            cmake .
-            cd cmake-build-debug || exit
+        for PROGRAM in "${PROGRAMS[@]}"; do
+
 
             # run each program 3 times, capturing time output
             TIMES=()
             for n in {1..3}; do
-                TIMES+=("$({ time ./main $FILE > /dev/null; } 2>&1 )")
+                TIMES+=("$({ time ./"$PROGRAM" "$FILE" > /dev/null; } 2>&1 )")
                 sleep 1
             done
 
             # Print the data to the console
-            printf "$FILE,$BRANCH,${DESCRIPTIONS[$BRANCH]},${TIMES[0]},${TIMES[1]},${TIMES[2]}\n"
+            printf "$FILE,$PROGRAM,${DESCRIPTIONS[$PROGRAM]},${TIMES[0]},${TIMES[1]},${TIMES[2]}\n"
 
             # Write the data to the CSV file
-            echo "$FILE,$BRANCH,${DESCRIPTIONS[$BRANCH]},${TIMES[0]},${TIMES[1]},${TIMES[2]}" >> runtimes.csv
+            echo "$FILE,$PROGRAM,${DESCRIPTIONS[$PROGRAM]},${TIMES[0]},${TIMES[1]},${TIMES[2]}" >> runtimes.csv
         done
     else
         echo "File $FILE does not exist."
---
+++ loop-unrolling.c
@@ -0,0 +1,175 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+
+#define HCAP (4096)
+#define BUFSIZE ((1 << 20) * 64)
+#define NO_CITY (-1)
+
+struct City {
+    char city[100];
+    int count;
+    double sum, min, max;
+};
+
+// hash returns a simple (but fast) hash for the first n bytes of data
+static unsigned int hash(const unsigned char *data, int n) {
+    unsigned int hash = 0;
+
+    for (int i = 0; i < n; i++) {
+        hash = (hash * 31) + data[i];
+    }
+
+    return hash;
+}
+
+static int compareStr(const void *ptr_a, const void *ptr_b) {
+    return strcmp(((struct City *) ptr_a)->city, ((struct City *) ptr_b)->city);
+}
+
+
+void print_results(struct City results[], int nresults) {
+    char buf[1024 * 16];
+    char *b = buf;
+    for (int i = 0; i < nresults; i++) {
+        b += sprintf(b, "%s=%.1f/%.1f/%.1f%s", results[i].city, results[i].min,
+                     results[i].sum / results[i].count, results[i].max,
+                     i < nresults - 1 ? ", " : "");
+    }
+    *b = 0x0;
+    puts(buf);
+}
+
+
+static const char *parse_city(int *len, unsigned int *hash, const char *s) {
+    int _len = 1;
+    unsigned int _hash = (unsigned char)s[0];
+
+    while (s[_len] != ';') {
+        _hash = (_hash * 31) + (unsigned char)s[_len++];
+    }
+
+    *len = _len;
+    *hash = _hash & (HCAP - 1);
+    return s + _len + 1;
+}
+
+/**
+ * parse_double - Parses a string into a double value.
+ * @dest: Pointer to a double where the parsed value will be stored.
+ * @s: Pointer to the string to be parsed.
+ *
+ * The function first checks if the first character of the string is a minus sign (-).
+ * If it is, it sets mod to -1.0 and increments the string pointer s to point to the next character.
+ * If the first character is not a minus sign, mod is set to 1.0.
+ * The function then checks if the second character of the string is a decimal point (.).
+ * If it is, it calculates the double value as the sum of the first character and the third character divided by 10.0,
+ * subtracts 1.1 * '0' to adjust for the ASCII value of '0', and multiplies the result by mod to account for a possible negative sign.
+ * The pointer s is then incremented by 4 to point to the character following the last character used in the conversion.
+ * If the second character is not a decimal point,
+ * the function calculates the double value as the sum of the first two characters times 10 plus the fourth character divided by 10.0,
+ * subtracts 11.1 * '0' to adjust for the ASCII value of '0', and multiplies the result by mod to account for a possible negative sign.
+ * The pointer s is then incremented by 5 to point to the character following the last character used in the conversion.
+ *
+ */
+static const char *parse_double(double *dest, const char *s) {
+    double mod;
+    if (*s == '-') {
+        mod = -1.0;
+        s++;
+    } else {
+        mod = 1.0;
+    }
+
+    if (s[1] == '.') {
+        *dest = (((double) s[0] + (double) s[2] / 10.0) - 1.1 * '0') * mod;
+        return s + 4;
+    }
+
+    *dest =
+            ((double) ((s[0]) * 10 + s[1]) + (double) s[3] / 10.0 - 11.1 * '0') * mod;
+    return s + 5;
+}
+
+
+int main(int argc, const char **argv) {
+    const char *file = "../data/measurements.txt";
+    if (argc > 1) {
+        file = argv[1];
+    }
+
+    FILE *fh = fopen(file, "r");
+    if (!fh) {
+        perror("error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    struct City results[450];
+    int nresults = 0;
+    char *buf = malloc(BUFSIZE);
+    if (buf == NULL || buf == MAP_FAILED) {
+        perror("mmap");
+        return EXIT_FAILURE;
+    }
+    int map[HCAP];
+    memset(map, -1, HCAP * sizeof(int));
+
+    const char *start;
+    char *pos;
+    double measurement;
+    unsigned int h;
+    int cityIdx;
+    int keylen;
+
+    while (1) {
+        size_t nread = fread(buf, 1, BUFSIZE, fh);
+        if (nread <= 0) {
+            break;
+        }
+
+        const char *s = buf;
+        long rewind = 0;
+        while (buf[nread - 1] != '\n') {
+            rewind--;
+            nread--;
+        }
+        fseek(fh, rewind, SEEK_CUR);
+
+        while (s < &buf[nread]) {
+            start = s;
+            s = parse_city(&keylen, &h, s);
+            s = parse_double(&measurement, s);
+            cityIdx = map[h];
+            while (cityIdx != NO_CITY &&
+                   memcmp(results[cityIdx].city, start, (size_t) keylen) != 0) {
+                h = (h + 1) & (HCAP - 1);
+                cityIdx = map[h];
+            }
+
+            if (cityIdx < 0) {
+                memcpy(results[nresults].city, start, (size_t) keylen);
+                results[nresults].city[keylen] = '\0';
+                results[nresults].sum = measurement;
+                results[nresults].max = measurement;
+                results[nresults].min = measurement;
+                results[nresults].count = 1;
+                map[h] = nresults;
+                nresults++;
+            } else {
+                results[cityIdx].sum += measurement;
+                results[cityIdx].count += 1;
+                if (results[cityIdx].min > measurement) {
+                    results[cityIdx].min = measurement;
+                } else if (results[cityIdx].max < measurement) {
+                    results[cityIdx].max = measurement;
+                }
+            }
+        }
+    }
+
+    qsort(results, (size_t) nresults, sizeof(*results), compareStr);
+
+    print_results(results, nresults);
+    fclose(fh);
+}
