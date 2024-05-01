0411b78a524e23412624cba70291785bc27a9f4d
Niklas <niklas.bognar@gmail.com>
2024-05-01 11:29:13 +0100

    added linear probing in v3

--- v2.c
+++ v2.c
@@ -2,19 +2,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-
-
+#define HCAP (4096)
 #define BUFSIZE (1 << 10)
+#define NO_CITY (-1)
+
 struct City {
     char city[100];
     int count;
     double sum, min, max;
 };
 
+// hash returns a simple (but fast) hash for the first n bytes of data
+static unsigned int hash(const unsigned char *data, int n) {
+    unsigned int hash = 0;
 
+    for (int i = 0; i < n; i++) {
+        hash = (hash * 31) + data[i];
+    }
+
+    return hash;
+}
 
 static int compareStr(const void *ptr_a, const void *ptr_b) {
     return strcmp(((struct City *)ptr_a)->city, ((struct City *)ptr_b)->city);
+}
+
+
+void printResults(const struct City *results, const int *resultCount) {
+    for (int i = 0; i < resultCount; i++) {
+        printf("%s=%.1f/%.1f/%.1f%s", results[i].city, results[i].min,
+               results[i].sum / results[i].count, results[i].max,
+               i < resultCount? ", " : "");
+    }
+    puts("\n");
 }
 
 
@@ -28,14 +48,6 @@ static int parseCity(const char *city, struct City results[], int resultCount) {
     return -1;
 }
 
-void printResults(const struct City *results, const int *resultCount) {
-    for (int i = 0; i < resultCount; i++) {
-        printf("%s=%.1f/%.1f/%.1f%s", results[i].city, results[i].min,
-               results[i].sum / results[i].count, results[i].max,
-               i < resultCount? ", " : "");
-    }
-    puts("\n");
-}
 
 int main(int argc, const char **argv) {
     const char *file = "../data/measurements.txt";
@@ -50,20 +62,30 @@ int main(int argc, const char **argv) {
     }
 
     struct City results[450];
-    int resultCount = 0;
+    int resultCount= 0;
     char buf[BUFSIZE];
+    int map[HCAP];
+    memset(map, NO_CITY, HCAP * sizeof(int));
+
     while (fgets(buf, 1 << 10, fh)) {
         char *pos = strchr(buf, ';');
         *pos = 0x0;
         double measurement = strtod(pos + 1, NULL);
 
-        int c = parseCity(buf, results, resultCount);
+        // find index of group by key through hash with linear probing
+        int h = hash((unsigned char *)buf, pos - buf) & (HCAP - 1);
+        while (map[h] != NO_CITY && strcmp(results[map[h]].city, buf) != 0) {
+            h = (h + 1) & (HCAP - 1);
+        }
+        int c = map[h];
+
         if (c < 0) {
             strcpy(results[resultCount].city, buf);
             results[resultCount].sum = measurement;
             results[resultCount].max = measurement;
             results[resultCount].min = measurement;
             results[resultCount].count = 1;
+            map[h] = resultCount;
             resultCount++;
         } else {
             results[c].sum += measurement;
@@ -77,7 +99,8 @@ int main(int argc, const char **argv) {
         }
     }
 
-    qsort(results, (size_t) resultCount, sizeof(*results), compareStr);
+    qsort(results, (size_t)resultCount, sizeof(*results), compareStr);
+
 
     printResults(results, &resultCount);
 
---
+++ v3.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define HCAP (4096)
+#define BUFSIZE (1 << 10)
+#define NO_CITY (-1)
+
+struct City {
+    char city[100];
+    int count;
+    double sum, min, max;
+};
+
+// hash returns a simple (but fast) hash for the first n bytes of data
+static unsigned int hash(const unsigned char *data, int n) {
+    unsigned int hash = 0;
+
+    for (int i = 0; i < n; i++) {
+        hash = (hash * 31) + data[i];
+    }
+
+    return hash;
+}
+
+static int compareStr(const void *ptr_a, const void *ptr_b) {
+    return strcmp(((struct City *)ptr_a)->city, ((struct City *)ptr_b)->city);
+}
+
+
+void printResults(const struct City *results, const int *resultCount) {
+    for (int i = 0; i < resultCount; i++) {
+        printf("%s=%.1f/%.1f/%.1f%s", results[i].city, results[i].min,
+               results[i].sum / results[i].count, results[i].max,
+               i < resultCount? ", " : "");
+    }
+    puts("\n");
+}
+
+/**
+ * parse_double - Parses a string into a double value.
+ * @dest: Pointer to a double where the parsed value will be stored.
+ * @s: Pointer to the string to be parsed.
+ *
+ * The function first checks if the first character of the string is a minus sign (-).
+ * If it is, it sets mod to -1.0 and increments the string pointer s to point to the next character.
+ * If the first character is not a minus sign, mod is set to 1.0.
+ * The function then checks if the second character of the string is a decimal point (.).
+ * If it is, it calculates the double value as the sum of the first character and the third character divided by 10.0,
+ * subtracts 1.1 * '0' to adjust for the ASCII value of '0', and multiplies the result by mod to account for a possible negative sign.
+ * The pointer s is then incremented by 4 to point to the character following the last character used in the conversion.
+ * If the second character is not a decimal point,
+ * the function calculates the double value as the sum of the first two characters times 10 plus the fourth character divided by 10.0,
+ * subtracts 11.1 * '0' to adjust for the ASCII value of '0', and multiplies the result by mod to account for a possible negative sign.
+ * The pointer s is then incremented by 5 to point to the character following the last character used in the conversion.
+ *
+ */
+static const char *parse_double(double *dest, const char *s) {
+    double mod;
+    if (*s == '-') {
+        mod = -1.0;
+        s++;
+    } else {
+        mod = 1.0;
+    }
+
+    if (s[1] == '.') {
+        *dest = (((double)s[0] + (double)s[2] / 10.0) - 1.1 * '0') * mod;
+        return s + 4;
+    }
+
+    *dest =
+            ((double)((s[0]) * 10 + s[1]) + (double)s[3] / 10.0 - 11.1 * '0') * mod;
+    return s + 5;
+}
+
+
+
+
+int main(int argc, const char **argv) {
+    const char *file = "../data/measurements.txt";
+    if (argc > 1) {
+        file = argv[1];
+    }
+
+    FILE *fh = fopen(file, "r");
+    if (!fh) {
+        perror("error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    struct City results[450];
+    int resultCount= 0;
+    char buf[BUFSIZE];
+    int map[HCAP];
+    memset(map, NO_CITY, HCAP * sizeof(int));
+
+    while (fgets(buf, BUFSIZE, fh)) {
+        char *pos = strchr(buf, ';');
+        *pos = 0x0;
+        double measurement;
+        parse_double(&measurement, pos + 1);
+
+        // find index of group by key through hash with linear probing
+        int h = hash((unsigned char *)buf, pos - buf) & (HCAP - 1);
+        while (map[h] != NO_CITY && strcmp(results[map[h]].city, buf) != 0) {
+            h = (h + 1) & (HCAP - 1);
+        }
+        int c = map[h];
+
+        if (c < 0) {
+            strcpy(results[resultCount].city, buf);
+            results[resultCount].sum = measurement;
+            results[resultCount].max = measurement;
+            results[resultCount].min = measurement;
+            results[resultCount].count = 1;
+            map[h] = resultCount;
+            resultCount++;
+        } else {
+            results[c].sum += measurement;
+            results[c].count += 1;
+            if (results[c].min > measurement) {
+                results[c].min = measurement;
+            }
+            if (results[c].max < measurement) {
+                results[c].max = measurement;
+            }
+        }
+    }
+
+    qsort(results, (size_t)resultCount, sizeof(*results), compareStr);
+
+
+    printResults(results, &resultCount);
+
+    fclose(fh);
+}
